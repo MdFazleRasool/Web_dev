a450abef4820345e4a663eae29dede7c83a82863
Mfrasool <mdfazlerasool786.com>
2024-10-13 11:10:49 +0530

    Tailwind Css and very basic javascript

---
+++ java_Script_basic/javaScript/Basic_000.js
@@ -0,0 +1,100 @@
+/* 
+console.log("and");
+console.log(0 && 1);
+
+console.log(10 && 301);
+
+console.log(0 && 30);
+
+console.log(10 && null);
+
+console.log(0 && null);
+
+console.log(null && 10);
+
+console.log("10" && -0);
+
+console.log("or");
+
+
+console.log(  0  || 1);
+console.log( 10  || 301);
+console.log( 0   || 30);
+console.log(10   || null);
+console.log( 0   || null);
+console.log(null || 10);
+console.log("10" || -0);
+*/
+/* 
+console.log(5 & 6);
+
+console.log(5 | 6);
+
+console.log(5 ^ 6);
+
+console.log(5 << 3);
+
+console.log(5 >> 3);
+
+console.log(100 >> 5);
+
+let i=0;
+do{
+    i++;
+}
+while(i>10);
+
+function greetingToEveryOne(){
+    console.log("My logic line 1");
+    console.log("My logic line 2");
+
+    console.log("My logic line 3");
+
+}
+greetingToEveryOne();
+greetingToEveryOne();
+
+console.log(Math.sqrt(100));
+
+function evenOrOdd(x){
+    console.log(x);
+    
+}
+evenOrOdd();
+*/
+/* 
+function even (x){
+    if(x % 2 ==0) return;
+    else return "odd";
+}
+
+let ans = even(11);
+console.log(ans);
+
+function add (x,y=10){
+    return x+y;
+}
+
+let add1 = add(10);
+console.log(add1);
+
+
+function unlimited(...values){
+    console.log(values);
+    
+}
+unlimited(1,2,3,4,5,6,7,8,9);
+unlimited(1);
+unlimited('f','a','z','l','e','r','a','s','o','o','l');
+
+console.log(Math.round(8.5));
+*/
+
+const d = new Date();
+console.log(d);
+console.log(d.getTime());
+console.log(d.getDate());
+console.log(d.getMonth());
+console.log(d.getFullYear());
+let x = `hii`;
+
