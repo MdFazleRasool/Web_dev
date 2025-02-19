62b3f7e99a512feb621f79d206f0dacfe1ae2734
Mfrasool <mdfazlerasool786.com>
2025-02-19 06:44:56 +0530

    Promise Ended

--- java_Script_basic/javaScript/Promise/008.js
+++ java_Script_basic/javaScript/Promise/008.js
@@ -1,8 +1,8 @@
 function download(url){
     return new Promise(function  exec(res , rej ){
         console.log("Started downloading data from " , url);
-        setTimeout(function (){
-            let data = " some data "  , url ;
+        setTimeout(function a(){
+            let data = " some data "  + url ;
             console.log("Downloaded data from ",url);
             res(data);
             
@@ -10,28 +10,30 @@ function download(url){
     } );
 }
 
+/* 
 download("https://www.example.com")
     .then(function f(value){
         console.log("Downloaded data is " , value); 
     })
+*/
 
 function writeFile(fata , fileName){
     return new Promise(function exec(res , rej){
-        setTimeout(function (){
+        setTimeout(function b(){
             console.log("Writing to file , " , fileName , "  is done");
-            let status = success;
+            let status = "success";
             res(status);
         },2000);
     });
 }
 
-function upload(fileName , url , callback){
+function upload(fileName , url ){
     return new Promise((resolve, reject) => {
         console.log("uploading file ", fileName , "to " , url);
-        setTimeout(function(){
+        setTimeout(function c(){
             console.log("Upload is done ");
             let uploadStatus = "Success";
-            res(uploadStatus)
+            reject(uploadStatus)
             
         },3000);
     })
@@ -58,22 +60,100 @@ upload("file.txt" , "https://www.example.com")
 
 
 */ 
+/*
+
+download --> waiting ffor downloading to complete  --> we execute function f --> f calls write file  ->
+when file writing is done p2 is resolved  --> then g is executed     --> g calls  upload -->when p3 is done
+p3 is resolved  -> then h is executed
+
+
+
+*/
+
+/*
 
 const p2 = download("https://www.example.com")
     .then(function handleDownload(value){
         console.log("Downloaded data is " , value);
-        return value 
+        return writeFile(value,"file.txt");
     });
 
 const p3=p2.then(function startWrite(value){
-    return writeFile(value , "file.txt");
+    console.log("file written" , value);
+    return upload(value , "https://www.example.com");
 })
 
-// const p4 = p3.then(function UploadFile("file.txt", "https://www.example.com",){
+p3.then(function h(value){
+    console.log("file upload " ,value);
     
-// })
+})
+
+*/
+// Another way of doing same things
+// .then() chaining
+
+/* 
+console.log("Start");
+
+const p5 = download("https://www.example.com")
+    .then(function handleDownload(value){
+        console.log("Downloaded data is " , value);
+        return writeFile(value,"file.txt");
+    },function fr(){
+        console.log("Download failed");
+        throw new Error("Download Failed")
+        
+    })
+    .then(function startWrite(value){
+        console.log("file written" , value);
+        return upload(value , "https://www.example.com");
+    },function gr(){
+        console.log("File write  failed");
+        throw new Error("File write Failed")
+        
+    })
+    .then(function uploading(value){
+        console.log("file upload " ,value);
+    },function hr(){
+        console.log("File upload  failed");
+        // throw new Error("File  upload Failed") ; // no need to throw here
+        
+    })
+    console.log("End");
+*/
+
+    console.log("Start");
+
+    const p7 = download("https://www.example.com")
+        .then(function handleDownload(value){
+            console.log("Downloaded data is " , value);
+            return writeFile(value,"file.txt");
+        })
+        .then(function startWrite(value){
+            console.log("file written" , value);
+            return upload(value , "https://www.example.com");
+        })
+        .then(function uploading(value){
+            console.log("file upload " ,value);
+        })
+        .catch(function i(value){
+            console.log("Error Occured" , value);
+            
+        }) // check it by making any  one of the 3 function or all to reject
+        console.log("End");
 
 
+
+
+
+/*
+ const p4 = p3.then(function UploadFile("file.txt", "https://www.example.com",){
+    
+ })
+*/
+
+
+/*
 const p4 = download("https://www.example.com")
     .then(function handleDownload(value){
         console.log("Downloaded data is " , value);
@@ -83,3 +163,5 @@ const p4 = download("https://www.example.com")
             },4000);
         })
     });
+
+*/
