9d404579b643be9280561b66f4b9620d56ad490d
Mfrasool <mdfazlerasool786.com>
2025-02-19 11:25:36 +0530

    return statement in the iterator function

--- java_Script_basic/javaScript/Async_JS_2/003Iterator(1).js
+++ java_Script_basic/javaScript/Async_JS_2/003Iterator(1).js
@@ -18,3 +18,20 @@ console.log(it.next(20));
 const x = it.next();
 const y = it.next(x.value);
 
+
+
+// When using return statement in between yields 
+
+function* generation(){
+    
+    console.log("Inside generator");
+    const x = yield 10;
+    return 88;
+    const y = x+30;
+    yield y;   
+}
+
+const itr = generation();
+const a = itr.next();
+const b = itr.next(a.value);
+itr.next();
